7f4574044c8d64f53b8e35c350743faf3d3b4980
Jean_Gabriel <2016069@crosemont.qc.ca>
2024-03-02 14:14:25 -0500

    Interaction avec les boutons plus au moins et changement en IMageBUtton

--- app/src/main/java/com/example/tic_tac_toe/Tic_Tac_Toc.kt
+++ app/src/main/java/com/example/tic_tac_toe/Tic_Tac_Toc.kt
@@ -1,15 +1,29 @@
 package com.example.tic_tac_toe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 
 class Tic_Tac_Toc : AppCompatActivity(), OnClickListener {
 
     lateinit var txt_resutlat : TextView
+    lateinit var btn1 : ImageButton
+    lateinit var btn2 : ImageButton
+    lateinit var btn3 : ImageButton
+    lateinit var btn4 : ImageButton
+    lateinit var btn5 : ImageButton
+    lateinit var btn6 : ImageButton
+    lateinit var btn7 : ImageButton
+    lateinit var btn8 : ImageButton
+    lateinit var btn9 : ImageButton
+    private lateinit var buttons: Array<ImageButton>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tic_tac_toc)
@@ -21,10 +35,31 @@ class Tic_Tac_Toc : AppCompatActivity(), OnClickListener {
 
         txt_resutlat.text = nom
 
+        buttons = arrayOf(
+            findViewById(R.id.btn1),
+            findViewById(R.id.btn2),
+            findViewById(R.id.btn3),
+            findViewById(R.id.btn4),
+            findViewById(R.id.btn5),
+            findViewById(R.id.btn6),
+            findViewById(R.id.btn7),
+            findViewById(R.id.btn8),
+            findViewById(R.id.btn9),
+
+        )
+
+        for (boutton in buttons ) {
+            boutton.setOnClickListener {
+                boutton.setBackgroundColor(Color.RED)
+            }
+        }
+
+
+
 
     }
 
     override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+
     }
 }
