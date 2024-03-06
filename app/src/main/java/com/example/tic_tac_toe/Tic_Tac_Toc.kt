3b1c64d5ccbbac1b4ad0104d9322c29d66d1e384
Jean_Gabriel <2016069@crosemont.qc.ca>
2024-03-06 01:11:36 -0500

    Tic tac toe fonctionnelle back-end

--- app/src/main/java/com/example/tic_tac_toe/Tic_Tac_Toc.kt
+++ app/src/main/java/com/example/tic_tac_toe/Tic_Tac_Toc.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
-import android.widget.Button
-import android.widget.GridLayout
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import com.example.tic_tac_toe.databinding.ActivityMainBinding
 import java.util.*
 
@@ -32,14 +29,15 @@ class Tic_Tac_Toc : AppCompatActivity(), OnClickListener {
 
     private var joueurTour : Boolean = true
 
-    var Player1 = ArrayList<Int>()
-    var Player2 = ArrayList<Int>()
+    var Player1 = mutableListOf<Int>()
+    var Player2 = mutableListOf<Int>()
 
-    private lateinit var binding : ActivityMainBinding
+    var ActivePlayer = 1
+    var setPlayer = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_tic_tac_toc)
 
 
@@ -87,15 +85,152 @@ class Tic_Tac_Toc : AppCompatActivity(), OnClickListener {
 
 
     override fun onClick(v: View?) {
+        var cellId = 0
         if (v is ImageButton) {
+            when(v?.id)
+            {
+                R.id.btn1 -> cellId = 1
+                R.id.btn2 -> cellId = 2
+                R.id.btn3 -> cellId = 3
+
+                R.id.btn4 -> cellId = 4
+                R.id.btn5 -> cellId = 5
+                R.id.btn6 -> cellId = 6
+
+                R.id.btn7 -> cellId = 7
+                R.id.btn8 -> cellId = 8
+                R.id.btn9 -> cellId = 9
+            }
             if (joueurTour ==true) {
                 v.setImageResource(androidx.appcompat.R.drawable.btn_radio_off_mtrl)
                 joueurTour= false
+                Player1.add(cellId)
                 v.isEnabled = false
+                CheckWinner()
+
+                println("Joueur 1"+" "+Player1.toString())
+
             } else if(joueurTour==false) {
                 v.setImageResource(androidx.constraintlayout.widget.R.drawable.abc_btn_default_mtrl_shape)
                 joueurTour=true
+                Player2.add(cellId)
+                println("Joueur 2"+" "+Player2.toString())
                 v.isEnabled = false
+                CheckWinner()
+            }
+        }
+    }
+
+    fun CheckWinner()
+    {
+        var winner = -1
+
+        //row1
+        if (Player1.contains(1) && Player1.contains(2) && Player1.contains(3))
+        {
+            winner = 1
+        }
+        if (Player2.contains(1) && Player2.contains(2) && Player2.contains(3))
+        {
+            winner = 2
+        }
+
+        //row2
+        if (Player1.contains(4) && Player1.contains(5) && Player1.contains(6))
+        {
+            winner = 1
+        }
+        if (Player2.contains(4) && Player2.contains(5) && Player2.contains(6))
+        {
+            winner = 2
+        }
+
+        //row3
+        if (Player1.contains(7) && Player1.contains(8) && Player1.contains(9))
+        {
+            winner = 1
+        }
+        if (Player2.contains(7) && Player2.contains(8) && Player2.contains(9))
+        {
+            winner = 2
+        }
+
+        //col1
+        if (Player1.contains(1) && Player1.contains(4) && Player1.contains(7))
+        {
+            winner = 1
+        }
+        if (Player2.contains(1) && Player2.contains(4) && Player2.contains(7))
+        {
+            winner = 2
+        }
+
+        //col2
+        if (Player1.contains(2) && Player1.contains(5) && Player1.contains(8))
+        {
+            winner = 1
+        }
+        if (Player2.contains(2) && Player2.contains(5) && Player2.contains(8))
+        {
+            winner = 2
+        }
+
+        //col3
+        if (Player1.contains(3) && Player1.contains(6) && Player1.contains(9))
+        {
+            winner = 1
+        }
+        if (Player2.contains(3) && Player2.contains(6) && Player2.contains(9))
+        {
+            winner = 2
+        }
+
+        //cross1
+        if (Player1.contains(1) && Player1.contains(5) && Player1.contains(9))
+        {
+            winner = 1
+        }
+        if (Player2.contains(1) && Player2.contains(5) && Player2.contains(9))
+        {
+            winner = 2
+        }
+
+        //cross2
+        if (Player1.contains(3) && Player1.contains(5) && Player1.contains(7))
+        {
+            winner = 1
+        }
+        if (Player2.contains(3) && Player2.contains(5) && Player2.contains(7))
+        {
+            winner = 2
+        }
+
+        if (winner != -1)
+        {
+            if (winner == 1)
+            {
+                if(setPlayer == 1) {
+                    Toast.makeText(this, "Player 1 Wins!!", Toast.LENGTH_SHORT).show()
+                    println("Joeur 1 a gagné")
+                }
+                else
+                {
+                    Toast.makeText(this, "You Won!!", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+            else
+            {
+                if (setPlayer == 1) {
+                    Toast.makeText(this, "Player 2 Wins!!", Toast.LENGTH_SHORT).show()
+                    println("Joeur 2 a gagné")
+
+                }
+                else
+                {
+                    Toast.makeText(this, "CPU Wins!!", Toast.LENGTH_SHORT).show()
+
+                }
             }
         }
     }
